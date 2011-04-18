e84c73e4c98e1641dc43ab8aa267318c524b5c4c
Fred Concklin <fred@fredri.cc>
2011-04-18 19:50:23 -0400

    moved things around

--- project.clj
+++ project.clj
@@ -13,9 +13,11 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
+		     [marginalia "0.5.0"]
 		     [lein-clojars "0.6.0"]]
   :min-lein-version "1.5.0"
-  :main leijush.gp1
+  ;; fix
+  :main ecopush.core
   ;; :aot [leijush.core]
   ;; :keep-non-project-classes true
   )
