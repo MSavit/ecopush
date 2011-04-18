10706ca7ae2845505e1d0c52ca0f4f8b4e7d366e
Fred Concklin <fred@fredri.cc>
2011-04-18 04:16:26 -0400

    moved mg into ecopush

--- project.clj
+++ project.clj
@@ -1,4 +1,21 @@
 (defproject ecopush "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Clojush with lein"
+  :url "https://github.com/fconcklin/leijush"
+  :license {:name "Gnu General Public License (GPL)"
+	    :url "http://www.gnu.org/licenses/gpl.html"
+	    :distribution :repo
+	    :comments "same as Clojush"}
+  :mailing-list {:name "push mailing list"
+		 :archive "https://lists.hampshire.edu/pipermail/push/"
+		 :post "push@lists.hampshire.edu"
+		 :subscribe "https://lists.hampshire.edu/mailman/listinfo/push"
+		 :unsubscribe "https://lists.hampshire.edu/mailman/listinfo/push"}
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [lein-clojars "0.6.0"]]
+  :min-lein-version "1.5.0"
+  :main leijush.gp1
+  ;; :aot [leijush.core]
+  ;; :keep-non-project-classes true
+  )
