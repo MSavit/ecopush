e42c805f2b1c6e08eaf1e67a7f518963895f37b5
Fred Concklin <fred@fredri.cc>
2011-05-20 02:39:04 -0400

    made additions to re

--- project.clj
+++ project.clj
@@ -10,9 +10,10 @@
 		 :post "ecopush@googlegroups.com"}
   :min-lein-version "1.5.0"
   :dependencies [[org.clojure/clojure "1.2.0"]
+		 [incanter "1.2.3"]
 		 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
 		     [marginalia "0.5.0"]
 		     [lein-clojars "0.6.0"]]
   ;; :jvm-opts ["-XmX1024m"]
-  :main ecopush.new)
+  :main ecopush.re)
--- src/ecopush/re.clj
+++ src/ecopush/re.clj
@@ -1,15 +1,18 @@
 (ns ecopush.re
-  [use [ecopush.push]])
+  [use [ecopush.push]
+   [clojure.contrib.math :only (round)]
+   [incanter.core]])
 
 (defn linear-payoff
   "linear payoff function"
   [d v r c m]
   (if (= d 0)
     v
-    (+ v (* r (- c m)))))
+    (+
+     v (* r (- c m)))))
 
 (defn smne
-  [i N]
+  [i N c]
   (/ (- c 1)
      (- N 1)))
 
@@ -17,8 +20,8 @@
   [plist c]
   (shuffle plist)
   (let [N (count plist)
-	j (random-nth (range (dec c))) 
-	k (random-nth (range (- N c)))
+	j (rand-nth (range (dec c))) 
+	k (rand-nth (range (- N c)))
 	r (- N j k)]
     (flatten (cons (repeat j 1)
 		   (repeat k 0)
@@ -34,8 +37,20 @@
   [propensities]
   (map linear-choice-rule propensities))
 
+;;; write this ;)
+(defn previous-propensity
+  "take the previus propensity for the player"
+  []
+  nil)
+
+;;; write this ;)
+(defn previous-strategy
+  "return the previous strategy"
+  []
+  nil)
+
 (defn simple-reinforcement
-  [n past-round]
+  [n past-round v r capacity previous-entrants]
   (list
    (+ (previous-propensity 1)
       (* previous-strategy
@@ -46,15 +61,15 @@
 	      v)))))
 
 (defn hypothetical-reinforcement
-  [n past-round]
+  [n past-round v r c previous-entrants]
   (list
    (+ (previous-propensity 1)
-      v
-      (* r
-	 (- c previous-entrants (- 1 previous-strategy))))
+      v (* r
+	   (- c previous-entrants (- 1 previous-strategy))))
    (+ (previous-propensity 0)
       v)))
 
+;;; round needs to be loaded 
 (defn activation
   [value]
   (if (not (number? value))
@@ -73,34 +88,52 @@
 		   (push-item gamelist :auxiliary))
 	      (run-push code)))))
 
+;v r c m
 (defn play
   [gamelist push strat]
-  (map #(linear-payoff %)
-       (flatten
-	(push-eval push gamelist)
-	(strat gamelist))))
+  (let [gamestate (cons
+		   (push-eval push gamelist)
+		   (map (fn [x] (x gamelist)) strat))
+	entered (apply + gamestate)]
+    (map #(linear-payoff % 1 1 20 entered)
+	 gamestate)))
 
+
+;;; include popsize
 (defn play-game
   [n gamelist push strat]
   (loop [r 0 g gamelist]
     (if (= n r)
-      gamelist
-      (recur (inc r) (play g push strat)))))
+      g
+      (recur (inc r) (conj g (play g push strat))))))
 
 (defn square
   [x]
   (* x x))
 
 (defn fitfn
-  [game-description]
+  [rounds]
   (fn [program]
-    (/ 1
-       (square (apply + (map first (play-game program)))))))
+    (list    (/ 1
+		(apply + (map first
+			      (play-game rounds
+					 '[]
+					 program
+					 (list (fn [x] (rand-int 2))
+					       (fn [x] (rand-int 2)))
+					 ;; (list (fn [x] 1)
+					 ;;       (fn [x] 0))
 
+					 )))))
+))
 
+(defn run [params]
+  (let [popsize (:population params)
+	capacity (:capacity params)
+	rounds (:rounds params)]
+    (pushgp
+     :error-function (fitfn rounds))))
 
-
-
-
-
-
+(run {:popsize 3
+	:capacity 20
+	:rounds 6})
